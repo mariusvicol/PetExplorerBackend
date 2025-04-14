845c18df241ce6f6c95d161c7513fdec10cec05c
Maria Tem <mariatemneanu04@gmail.com>
2025-04-14 15:43:41 +0300

    repo impl for PensiuneCaninaRepository & SalonRepository

--- PetExplorerMain/PetExplorerPersistence/src/main/java/petexplorer/Main.java
+++ PetExplorerMain/PetExplorerPersistence/src/main/java/petexplorer/Main.java
@@ -2,10 +2,16 @@ package petexplorer;
 
 import petexplorer.domain.Farmacie;
 import petexplorer.domain.Magazin;
+import petexplorer.domain.PensiuneCanina;
+import petexplorer.domain.Salon;
 import petexplorer.farmacierepos.FarmacieVeterinaraRepository;
 import petexplorer.farmacierepos.IFarmacieVeterinaraRepository;
 import petexplorer.magazinerepos.IMagazinVeterinarRepository;
 import petexplorer.magazinerepos.MagazinVeterinarRepository;
+import petexplorer.pensiunecaninarepos.IPensiuneCaninaRepository;
+import petexplorer.pensiunecaninarepos.PensiuneCaninaRepository;
+import petexplorer.salonrepos.ISalonRepository;
+import petexplorer.salonrepos.SalonRepository;
 import petexplorer.utils.HibernateUtils;
 
 public class Main {
@@ -20,6 +26,23 @@ public class Main {
         for (Farmacie f : farmacieVeterinaraRepository.findAll()) {
             System.out.println(f);
         }
+
+        System.out.println("---------------------------\n");
+
+        IPensiuneCaninaRepository pcRepo = new PensiuneCaninaRepository(HibernateUtils.getSessionFactory());
+        ISalonRepository salonRepo = new SalonRepository(HibernateUtils.getSessionFactory());
+
+        System.out.println("Toate pensiunile:");
+        for (PensiuneCanina pc : pcRepo.findAll()) {
+            System.out.println(pc);
+        }
+
+        System.out.println("Toate saloanele:");
+        for (Salon s : salonRepo.findAll()) {
+            System.out.println(s);
+        }
+
+        System.out.println("----------------------------\n");
         HibernateUtils.getSessionFactory().close();
     }
 }
