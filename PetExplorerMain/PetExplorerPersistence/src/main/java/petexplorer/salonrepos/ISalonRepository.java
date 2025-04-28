3f96fc517ac794d0ffdc267960ea9e47cb970942
Maria Tem <mariatemneanu04@gmail.com>
2025-04-28 17:57:16 +0300

    springframework + salon/pensiune controller

--- PetExplorerMain/PetExplorerPersistence/src/main/java/petexplorer/salonrepos/ISalonRepository.java
+++ PetExplorerMain/PetExplorerPersistence/src/main/java/petexplorer/salonrepos/ISalonRepository.java
@@ -1,7 +1,9 @@
 package petexplorer.salonrepos;
 
+import org.springframework.stereotype.Component;
 import petexplorer.Repository;
 import petexplorer.domain.Salon;
 
+@Component
 public interface ISalonRepository extends Repository<Integer, Salon> {
 }
