5dc6d23b448410a7c54ee3e4324706130426b7ca
Denis <antonio.denis2004@gmail.com>
2025-12-07 20:20:03 +0200

    Add utils for notification system and notificationRadius property

---
+++ PetExplorerMain/PetExplorerNetworking/src/main/java/petexplorer/notification/utils/ProximityFilterService.java
@@ -0,0 +1,31 @@
+package petexplorer.notification.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import petexplorer.service.UserLocationService;
+import petexplorer.utils.UserLocationVO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProximityFilterService {
+    private final UserLocationService userLocationService;
+    private final DistanceCalculator distanceCalculator;
+
+    @Value("${notification.radius.default}")
+    private double notificationRadius;
+
+    public ProximityFilterService(UserLocationService userLocationService, DistanceCalculator distanceCalculator) {
+        this.userLocationService = userLocationService;
+        this.distanceCalculator = distanceCalculator;
+    }
+
+    public Iterable<Integer> getUsersInProximity(double eventLatitude, double eventLongitude) {
+        List<Integer> usersInProximity = new ArrayList<>();
+        for (UserLocationVO userLocation: userLocationService.getUserLocations()) {
+            if (distanceCalculator.getDistance(userLocation.getLatitude(), userLocation.getLongitude(), eventLatitude, eventLongitude) <= notificationRadius) {
+                usersInProximity.add(userLocation.getUserId());
+            }
+        }
+        return usersInProximity;
+    }
+}
